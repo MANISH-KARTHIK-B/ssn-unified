646f7390cbb51b2202fa48fd30bcc1966756a5d1
MANISH-KARTHIK-B <manishkarthikb@gmail.com>
2026-07-26 13:58:19 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,10 +1,13 @@
 import { Low } from "lowdb";
 import { JSONFile } from "lowdb/node";
 import path from "path";
+import fs from "fs";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const file = path.join(__dirname, "data", "db.json");
+const dataDir = path.join(__dirname, "data");
+fs.mkdirSync(dataDir, { recursive: true });
+const file = path.join(dataDir, "db.json");
 const adapter = new JSONFile(file);
 
 export const db = new Low(adapter, {});
